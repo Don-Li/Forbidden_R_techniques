11ae3b9913f4fa0f75f2393eb9cea064709f197b
Don-Li <yli877@aucklanduni.ac.nz>
2020-03-11 23:06:31 +1300

    compile forbidden rituals

---
+++ compile_forbidden_rituals.R
@@ -0,0 +1,12 @@
+file.remove("Forbidden_techniques.txt")
+
+R_files = list.files(pattern = ".R")
+R_files = R_files[ !grepl( ".Rproj", R_files ) ]
+
+file_ = R_files[1]
+for ( file_ in R_files ){
+    text_ = paste0( "#####", file_, "#####\n" )
+    text_ = c( text_, suppressWarnings( readLines( file_ ) ) )
+    text_ = c( text_, "########\n" )
+    cat( text_, file = "Forbidden_techniques.txt", append = TRUE, sep = "\n")
+}
